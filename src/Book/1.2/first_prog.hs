17dd08c8b670bd06048e75663da28e31cbe75f62
admiraloom <roar.holte@youblob.com>
2024-01-06 17:51:33 +0100

    Back to school

---
+++ src/Book/1.2/first_prog.hs
@@ -0,0 +1,16 @@
+messyMain :: IO ()
+messyMain = do
+  print "Who is this email for?"
+  recipient <- getLine
+  print "What is the title?"
+  title <- getLine
+  print "Who is the Author?"
+  author <- getLine
+  print
+    ( "Dear " ++ recipient ++ ",\n"
+        ++ "Thanks for buying "
+        ++ title
+        ++ "\nthanks,\n"
+        ++ author
+    )
+toPart recipient = "Dear " ++ recipient ++ ",\n"
---
+++ src/Book/hello.hs
@@ -0,0 +1,3 @@
+-- some text
+main = do
+  print "Hello Roar aka Admiral OOM! Great to have you here."
